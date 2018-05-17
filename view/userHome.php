5a0f7e0f71b9ce070fc1d3ebf578c1cb10cd6a04
Justinio99 <31925156+Justinio99@users.noreply.github.com>
2018-05-17 11:29:45 +0200

    schgumpe
    add Style

--- view/userHome.php
+++ view/userHome.php
@@ -5,15 +5,17 @@ echo "<p>Willkommen ".$_SESSION['userName']."</p>";
 
 ?>
 
-<button id="addGallerie">Gallerie Hinzufügen</button>
+
+<a id="addGallerie" class="waves-effect waves-light btn-small"><i id="addGallerie" class="material-icons right">add</i>Gallerie Hinzufügen</a>
 <form action="/N-J_Gibb/public/galleries/createGallerie" method="post" class="gallerieForm hidden">
 <label>Name Gallerie</label>
 <input type="text" name="gallerieName"><br>
 <label>Beschreibung</label>
 <input type="text" name="beschreiubng">
-<button id="submitGallerie" type="submit">Erstellen</button>
+<button id="submitGallerie" class="waves-effect waves-light btn-small" type="submit"><i class="material-icons right">arrow_forward</i>Erstellen</button>
 </form>
 
 <?php
 
 ?>
+
